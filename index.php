927b9af97d5648d9a2297991860dd6b9f077b1ae
Mamba1099 <mambakaranja240@gmail.com>
2023-09-04 00:28:17 +0300

    learning pdo

--- index.php
+++ index.php
@@ -1,109 +1,47 @@
 <?php
-$host =  'localhost';
-$user = 'root';
-$password = '123456';
-$dbname = 'pdoposts';
 
-// Set DSN
-$dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+$host = "localhost";
+$user = "root";
+$password = "Mamba@100";
+$dbname = "Pdopost";
 
-// Create a PDO instance
+// Set DSN (Data Source Name)
+$dsn = "mysql:host=" . $host . ";dbname=" . $dbname;
+
+
+// Create a new PDO instance
 $pdo = new PDO($dsn, $user, $password);
+
+// Set the default fetch mode to PDO::FETCH_OBJ
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-# PRDO QUERY
+// PDO query: Select all columns from the "Post" table
+//     $stmt = $pdo->query("SELECT * FROM Post");
 
-// $stmt = $pdo->query('SELECT * FROM posts');
-
-// while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-//   echo $row['title'] . '<br>';
+// Loop through the result set
+//     while ($row = $stmt->fetch()) {
+//         echo $row->title . "<br>";
+//     }
+// } catch (PDOException $e) {
+//     // Handle any exceptions (e.g., database connection error)
+//     echo "Error: " . $e->getMessage();
 // }
 
-// while($row = $stmt->fetch()){
-//   echo $row->title . '<br>';
-// }
-
-# PREPARED STATEMENTS (prepare & execute)
-
-// UNSAFE
-//$sql = "SELECT * FROM posts WHERE author = '$author'";
-
-// FETCH MULTIPLE POSTS
-
-// User Input
-$author = 'Brad';
+$author = "Brad";
 $is_published = true;
-$id = 1;
-$limit = 1;
 
-// Positional Params
-$sql = 'SELECT * FROM posts WHERE author = ? && is_published = ? LIMIT ?';
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$author, $is_published, $limit]);
+
+$sql = "SELECT * FROM Post WHERE author= ;author && is _published = :is_published";
+$stml = $pdo->prepare($sql);
+$stml->execute(["author" => $author, "is_published" => $is_published]);
 $posts = $stmt->fetchAll();
 
-// Named Params
-// $sql = 'SELECT * FROM posts WHERE author = :author && is_published = :is_published';
-// $stmt = $pdo->prepare($sql);
-// $stmt->execute(['author' => $author, 'is_published' => $is_published]);
-// $posts = $stmt->fetchAll();
+var_dump($posts);
 
-// //var_dump($posts);
-foreach ($posts as $post) {
-    echo $post->title . '<br>';
+if ($posts) {
+    foreach ($posts as $post) {
+        echo $post->title . "<br>";
+    }
+} else {
+    echo "No post found";
 }
-
-  // FETCH SINGLE POST
-
-  // $sql = 'SELECT * FROM posts WHERE id = :id';
-  // $stmt = $pdo->prepare($sql);
-  // $stmt->execute(['id' => $id]);
-  // $post = $stmt->fetch();
-
-  // echo $post->body;
-
-  // GET ROW COUNT
-  // $stmt = $pdo->prepare('SELECT * FROM POSTS WHERE author = ?');
-  // $stmt->execute([$author]);
-  // $postCount = $stmt->rowCount();
-
-  // echo $postCount;
-
-  // INSERT DATA
-  // $title = 'Post Five';
-  // $body = 'This is post five';
-  // $author = 'Kevin';
-
-  // $sql = 'INSERT INTO posts(title, body, author) VALUES(:title, :body, :author)';
-  // $stmt = $pdo->prepare($sql);
-  // $stmt->execute(['title' => $title, 'body' => $body, 'author' => $author]);
-  // echo 'Post Added';
-
-  // UPDATE DATA
-  // $id = 1;
-  // $body = 'This is the updated post';
-
-  // $sql = 'UPDATE posts SET body = :body WHERE id = :id';
-  // $stmt = $pdo->prepare($sql);
-  // $stmt->execute(['body' => $body, 'id' => $id]);
-  // echo 'Post Updated';
-
-  // DELETE DATA
-  // $id = 3;
-
-  // $sql = 'DELETE FROM posts WHERE id = :id';
-  // $stmt = $pdo->prepare($sql);
-  // $stmt->execute(['id' => $id]);
-  // echo 'Post Deleted';
-
-  // SEARCH DATA
-  // $search = "%f%";
-  // $sql = 'SELECT * FROM posts WHERE title LIKE ?';
-  // $stmt = $pdo->prepare($sql);
-  // $stmt->execute([$search]);
-  // $posts = $stmt->fetchAll();
-
-  // foreach($posts as $post){
-  //   echo $post->title . '<br>';
-  // }
